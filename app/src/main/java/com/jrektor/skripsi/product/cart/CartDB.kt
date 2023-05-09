a7bea5cbd47dcb8894b240f3d8f9ecd3cf1dae97
Ichwan Sholihin <ichwansholihin03@gmail.com>
2023-05-09 21:18:59 +0700

    smoothing layout

--- app/src/main/java/com/jrektor/skripsi/product/cart/CartDB.kt
+++ app/src/main/java/com/jrektor/skripsi/product/cart/CartDB.kt
@@ -6,7 +6,8 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.jrektor.skripsi.product.items.ModelProduct
 
-@Database(entities = [ModelProduct::class], version = 1)
+
+@Database(entities = [ModelProduct::class], version = 1, exportSchema = false)
 abstract class CartDB : RoomDatabase() {
     abstract fun daoCart(): DaoCart
     companion object {
